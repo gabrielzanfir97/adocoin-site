7cea836eb225a836873c861b14895f365db7ab4f
gabrielzanfir97 <gabrielzanfir97@gmail.com>
2025-06-09 15:33:29 +0100

    Delete pages directory

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ padding: "2rem", fontFamily: "Arial" }}>
-      <h1>Bine ai venit pe site-ul oficial Adocoin!</h1>
-      <p>Tokenul românesc bazat pe tehnologia blockchain.</p>
-    </div>
-  );
-}
